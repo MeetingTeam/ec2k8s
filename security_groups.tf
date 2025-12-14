2ad8d644add1168cb2584b7cab6d0d2e581f33c2
betterguy2004 <22520524@gm.uit.edu.vn>
2025-12-14 14:27:09 +0700

    add securitygroup

--- security_groups.tf
+++ security_groups.tf
@@ -65,6 +65,22 @@ resource "aws_security_group" "k8s_master" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "Http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "Https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
