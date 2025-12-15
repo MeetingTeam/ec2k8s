892ab42df2cc696fa5efc66529805ab8a974a419
betterguy2004 <22520524@gm.uit.edu.vn>
2025-12-15 21:04:59 +0700

    install app guide txt

--- backinstall.sh
+++
@@ -1,9 +0,0 @@
-
-helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
-helm repo update
-helm install ebs-csi aws-ebs-csi-driver/aws-ebs-csi-driver \
-  --namespace kube-system \
-  --set controller.serviceAccount.create=true \
-  --set controller.serviceAccount.name=ebs-csi-controller-sa
-
-## instal EBS storage class
