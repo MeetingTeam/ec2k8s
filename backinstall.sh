079dd4c696bb0fb7f9960d7e8b8490d05021ed97
betterguy2004 <22520524@gm.uit.edu.vn>
2025-12-15 20:11:24 +0700

    clean code

--- backinstall.sh
+++ backinstall.sh
@@ -1,10 +1,3 @@
-git clone https://github.com/kubernetes/cloud-provider-aws.git
-cd cloud-provider-aws/examples/existing-cluster/base
-kubectl create -k .
-kubectl get daemonset -n kube-system
-kubectl get pods -n kube-system
-
-kubectl get nodes -o wide
 
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
 helm repo update
@@ -13,3 +6,4 @@ helm install ebs-csi aws-ebs-csi-driver/aws-ebs-csi-driver \
   --set controller.serviceAccount.create=true \
   --set controller.serviceAccount.name=ebs-csi-controller-sa
 
+## instal EBS storage class
